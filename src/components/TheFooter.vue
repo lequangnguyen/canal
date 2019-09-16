786903a2371b0f09c01a589cd98bccad0970bd28
nguyen <NguyenLQ@daivietgroup.net.vn>
2019-09-16 17:52:13 +0700

    update App.vue

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer>
+        <div class="container">
+            <a href="/" class="logo-font">canal</a>
+            <span class="attribution">
+                An interactive learning project.
+            </span>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+};
+</script>
+
+<style scoped></style>
